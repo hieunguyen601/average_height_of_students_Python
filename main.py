b10ea5bc3203ab6445a9c90bafbeba62714fd5b4
Hieu Nguyen <hieunguyen@Hieus-MacBook-Pro.local>
2024-02-21 16:58:29 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+student_heights = [180, 170, 165, 162, 176]
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+total_height = 0
+for height in student_heights:
+    total_height += height
+print(f"total height = {total_height}")
+
+number_of_students = 0
+for student in student_heights:
+    number_of_students += 1
+print(f"number of students = {number_of_students}")
+
+average = total_height / number_of_students
+print(average)
